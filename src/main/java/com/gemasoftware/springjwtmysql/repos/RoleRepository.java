24c4b92415ae4de607c70b7b3720bc4ef245b45b
Agron Gemajli <agrongemajli@outlook.com>
2020-07-20 12:14:52 -0400

    Added Role Repository that extends JpaRepository to find a role based on its role name.

---
+++ src/main/java/com/gemasoftware/springjwtmysql/repos/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.gemasoftware.springjwtmysql.repos;
+
+import com.gemasoftware.springjwtmysql.models.Role;
+import com.gemasoftware.springjwtmysql.models.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByUserRoleName(UserRole userRoleName);
+
+}
