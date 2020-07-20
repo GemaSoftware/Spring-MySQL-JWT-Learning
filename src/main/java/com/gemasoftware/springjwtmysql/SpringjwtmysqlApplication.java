68887b7c4830ef9a40c8bfbab25016177e699995
Agron Gemajli <agrongemajli@outlook.com>
2020-07-20 11:27:18 -0400

    Initial commit

---
+++ src/main/java/com/gemasoftware/springjwtmysql/SpringjwtmysqlApplication.java
@@ -0,0 +1,14 @@
+package com.gemasoftware.springjwtmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjwtmysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjwtmysqlApplication.class, args);
+    }
+
+
+}
